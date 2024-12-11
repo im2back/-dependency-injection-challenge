5335aac226f344e7c2a7788918f0c3ac46b71203
im2back <jefferson.richards2@outlook.com>
2024-12-11 17:33:15 -0300

    Adicionando saída

--- src/main/java/com/jeff_dev/devsuperior/DevsuperiorApplication.java
+++ src/main/java/com/jeff_dev/devsuperior/DevsuperiorApplication.java
@@ -23,12 +23,36 @@ public class DevsuperiorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		Order order = new Order("1034", new BigDecimal(150),20);
-		var result = orderService.calc(order);
-
-		System.out.println("Code: "+result.code());
-		System.out.println("Total Value: "+ result.totalValue());
+		
+		System.out.println();
+		//pedido1
+		System.out.println("## Pedido 1 ##");
+		Order order1 = new Order("1034", new BigDecimal(150),20);
+		var calc1= orderService.calc(order1);
+		
+		System.out.println("Pedido código "+ calc1.code());
+		System.out.println("Valor total: R$ "+ calc1.totalValue());
+		System.out.println();
+		
+		//pedido2
+		System.out.println("## Pedido 2 ##");
+		Order order2 = new Order("2282", new BigDecimal(800),10);
+		var calc2= orderService.calc(order2);
+		
+		System.out.println("Pedido código "+ calc2.code());
+		System.out.println("Valor total: R$ "+ calc2.totalValue());
+		System.out.println();
+		
+		//pedido3
+		System.out.println("## Pedido 3 ##");
+		Order order3 = new Order("1309", new BigDecimal(95.90),0);
+		var calc3= orderService.calc(order3);
+		
+		System.out.println("Pedido código "+ calc3.code());
+		System.out.printf("Valor total: R$ %.2f%n", calc3.totalValue());
+		System.out.println();
+		
+		
 
 	}
 }
